20140fb6c7ef0cd2f600713c79c7308e3847bcf7
choim3 <choim3@spu.edu>
2020-11-17 19:08:12 -0800

    commit tree b04dc3fc4b9882d461338729d65c7f3845f7c593

---
+++ lib/dog.rb
@@ -0,0 +1,22 @@
+class Dog
+    # def initiallize(string)
+    #     @name = string
+    # end
+
+    def name=(dog_name)
+        @name = dog_name
+    end
+    
+    def name
+        @name
+    end
+
+    def breed=(dog_breed)
+        @breed = dog_breed
+    end
+
+    def breed 
+        @breed
+    end
+
+end
